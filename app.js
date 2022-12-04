0d8532d550a13ee09c8045b103a545d2a49e5f51
Saba <saba.sadegh67@gmail.com>
2022-12-04 13:51:11 +0330

    creating javascript file and add functionality to buttons

---
+++ app.js
@@ -0,0 +1,26 @@
+let count = 0;
+const value = document.getElementById("value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const style = e.currentTarget.classList;
+    if (style.contains("increase")) {
+      count++;
+    } else if (style.contains("decrease")) {
+      count--;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
